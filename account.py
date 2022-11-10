fb62a6b01cf5fd30abfe5491aa7ae760e16176d5
Gareth Moodley <garethkm25@gmail.com>
2022-11-10 12:22:27 -0600

    getter methods

--- account.py
+++ account.py
@@ -3,3 +3,8 @@ class Account:
 		self.__account_name = name
 		self.__account_balance = 0
 		
+	def get_name(self):
+		return self.__account_name
+
+	def get_balance(self):
+		return self.__account_balance
