42b016cfc5691ee783bc22e206d06cdd7e5403df
Gareth Moodley <garethkm25@gmail.com>
2022-11-10 12:32:10 -0600

    finished account.py

--- account.py
+++ account.py
@@ -2,6 +2,20 @@ class Account:
 	def __init__(self, name):
 		self.__account_name = name
 		self.__account_balance = 0
+
+	def deposit(self, amount):
+		if amount <= 0:
+			return False
+		else:
+			self.__account_balance += amount
+			return True
+
+	def withdraw(self, amount);
+		if amount <= 0 or amount > self.__account_balance:
+			return False
+		else:
+			self.__account_balance -= amount
+			return True
 		
 	def get_name(self):
 		return self.__account_name
